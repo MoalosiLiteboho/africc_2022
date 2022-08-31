31a30dbe16c778bea488548c9a5f2fecdd3e4faf
MoalosiLiteboho <112495633+MoalosiLiteboho@users.noreply.github.com>
2022-08-31 09:40:48 +0200

    Update ProblemThree.py

--- Moalosi Liteboho/Assignment One/ProblemThree.py
+++ Moalosi Liteboho/Assignment One/ProblemThree.py
@@ -1,10 +1,10 @@
 def main():
     while True:
         sentence = input("Enter the sentence of your choice \nSentence > ")  # Accepting user-input and store it
-        words = sentence.split()
-        words.sort()
+        words = sentence.split()  # Statement that split sentence into words and stores those words in a list called words
+        words.sort()  # Sorting the words in a list alphabetically
 
-        for word in words:
+        for word in words:  # Printing the words using the loop
             print(word)
 
         choice = input("Do you want to try again? \nAnswer > ")  # Accept user-input whether user want to run code again
