c53010b264b28af0b4d77b02ee92d52b1f0df28e
MoalosiLiteboho <112495633+MoalosiLiteboho@users.noreply.github.com>
2022-08-31 10:20:20 +0200

    Update ProblemThree.py

--- Moalosi Liteboho/Assignment One/ProblemThree.py
+++ Moalosi Liteboho/Assignment One/ProblemThree.py
@@ -1,12 +1,12 @@
 def main():
     while True:
         sentence = input("Enter the sentence of your choice \nSentence > ")  # Accepting user-input and store it
+        sentence = sentence.lower()  # Statement that changes string in sentense variable into lower case
         words = sentence.split()  # Statement that split sentence into words and stores those words in a list called words
         words.sort()  # Sorting the words in a list alphabetically
+        final_sentence = ' '.join(words)  # Turning the list called words into a String with a space between words
 
-        for word in words:  # Printing the words using the loop
-            print(word)
-
+        print(final_sentence.capitalize())  # Capitalizing the fist letter of the string and printing the string 
         choice = input("Do you want to try again? \nAnswer > ")  # Accept user-input whether user want to run code again
 
         if choice.lower().startswith("y"):  # Check user input whether it start with y for yes, run code if it is
