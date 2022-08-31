dce1919e1baea3319ea1411ce16997a4faf4258a
MoalosiLiteboho <112495633+MoalosiLiteboho@users.noreply.github.com>
2022-08-31 09:38:24 +0200

    Update ProblemTwo.py

--- Moalosi Liteboho/Assignment One/ProblemTwo.py
+++ Moalosi Liteboho/Assignment One/ProblemTwo.py
@@ -1,15 +1,15 @@
-import os
+import os  # Importing the module named os
 
 
 def main():
     while True:
-        month = int(input("Enter the month in number: "))
-        year = int(input("Enter the year: "))
+        month = int(input("Enter the month in number: "))  # Accepting the month as int from the user and store it in a variable
+        year = int(input("Enter the year: "))  # Accepting the year as int from the user and store it in a variable
 
-        if 0 < month <= 12:
-            print("\nDisplaying Month Calender: \n")
-            month_calender = os.system("cal " + str(month) + " " + str(year))
-            print(month_calender)
+        if 0 < month <= 12:  #  If-statement that checks if the value in month is greater than zero and less than or equal to 12
+            print("\nDisplaying Month Calender: \n")  # The statement that print message before displaying calender month
+            month_calender = os.system("cal " + str(month) + " " + str(year))  # Statement that store bash command in a variable that will be runned when it is invocked 
+            print(month_calender)  # The statement that print the callender month by invocking the variable that will run bash command
 
         choice = input("Do you want to use the program again? : \nChoice > ")  # Accepting user input
 
